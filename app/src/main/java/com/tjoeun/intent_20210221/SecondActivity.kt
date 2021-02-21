21f249bce73193c61af8c35748c149c863b7d669
seoyeongkang <jumprope123@naver.com>
2021-02-21 14:03:31 +0900

    String 기본값 주석 추가

--- app/src/main/java/com/tjoeun/intent_20210221/SecondActivity.kt
+++ app/src/main/java/com/tjoeun/intent_20210221/SecondActivity.kt
@@ -12,8 +12,15 @@ class SecondActivity : AppCompatActivity() {
 //        목적지에 도착한 상황. => 첨부한 데이터를 받아주자. (어떤 형태의 자료인가?)
         val receivedMessage = intent.getStringExtra("message")
 
+//        String 등 참조형 변수는 기본값 지정
+
+        
 //        수신문구 표현 텍스트뷰의 문구로 설정 => 상황마다 코드가 달라짐
         receivedMessageTxt.text = receivedMessage
+
+//        첨부된 출생년도 받는 예시 => Int, Double, Boolean 등 기본형(에 가까운) 변수들은,
+//        첨부가 안됐을떼 뭘로 꺼낼지 기본값 지정 필요
+        val myBirthYear = intent.getIntExtra("출생연도", defaultValue:0)
 
 
     }
